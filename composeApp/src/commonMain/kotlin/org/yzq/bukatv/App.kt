75b146bc1899a8fe945cbadd1e9482a740365280
yzq <zhenqiang.yu@foxmail.com>
2025-07-05 17:51:09 +0800

    add 增加主屏幕、媒体库、设置页面

--- composeApp/src/commonMain/kotlin/org/yzq/bukatv/App.kt
+++ composeApp/src/commonMain/kotlin/org/yzq/bukatv/App.kt
@@ -1,96 +1,67 @@
 package org.yzq.bukatv
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Mediation
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import bukatv.composeapp.generated.resources.Res
-import bukatv.composeapp.generated.resources.compose_multiplatform
+sealed class Screen(
+    val labe: String,
+    val icon: ImageVector,
+    val content: @Composable (() -> Unit)
+) {
+    object Main : Screen("主屏幕", Icons.Default.PlayArrow, { MainScreen() })
+    object MediaLibrary : Screen("媒体库", Icons.Default.Search, { MediaLibraryScreen() })
+    object Settings : Screen("设置", Icons.Default.Settings, { SettingsScreen() })
+}
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        // 定义可变状态
-        var showContent by remember { mutableStateOf(false) }
-        var selectedTab by remember { mutableStateOf(0) }
+    val screens = listOf(
+        Screen.Main,
+        Screen.MediaLibrary,
+        Screen.Settings
+    )
+    // 定义可变状态
+    var screenTab by remember { mutableStateOf(0) }
 
+    MaterialTheme {
         Scaffold(
-            modifier =  Modifier
+            modifier = Modifier
                 .fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    title = { Text("媒体库") }
-                )
-            },
-            content = { paddingValues -> Column(
-                    modifier = Modifier.padding(paddingValues).fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Button(onClick = { showContent = !showContent }) {
-                        Text("Click me!")
-                    }
-                    AnimatedVisibility(showContent) {
-                        val greeting = remember { Greeting().greet() }
-                        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                            Image(painterResource(Res.drawable.compose_multiplatform), null)
-                            Text("Compose: $greeting")
-                        }
-                    }
-                }
-            },
             bottomBar = {
                 NavigationBar {
-                    NavigationBarItem(
-                        selected = selectedTab == 0,
-                        onClick = { selectedTab = 0 },
-                        icon = { Icon(Icons.Default.PlayArrow, contentDescription = "媒体库") },
-                        label = { Text("媒体库") }
-
-                    )
-                    NavigationBarItem(
-                        selected = selectedTab == 1,
-                        onClick = { selectedTab = 1 },
-                        icon = { Icon(Icons.Default.Search, contentDescription = "搜索") },
-                        label = { Text("搜索") }
-
-                    )
-                    NavigationBarItem(
-                        selected = selectedTab == 2,
-                        onClick = { selectedTab = 2 },
-                        icon = { Icon(Icons.Default.Settings, contentDescription = "设置") },
-                        label = { Text("设置") }
-
-                    )
+                    screens.forEachIndexed { index, screen ->
+                        NavigationBarItem(
+                            selected = screenTab == index,
+                            onClick = { screenTab = index },
+                            icon = { Icon(screen.icon, contentDescription = screen.labe) },
+                            label = { Text(screen.labe) }
+                        )
+                    }
                 }
             }
-
-        )
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                screens[screenTab].content()
+            }
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/org/yzq/bukatv/MediaLibraryScreen.kt
@@ -0,0 +1,65 @@
+package org.yzq.bukatv
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import bukatv.composeapp.generated.resources.Res
+import bukatv.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun MediaLibraryScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // 定义可变状态
+        var showContent by remember { mutableStateOf(false) }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("媒体库") }
+                )
+            },
+            content = { innerPadding ->
+                Column(
+                    modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Button(onClick = { showContent = !showContent }) {
+                        Text("Click me!")
+                    }
+                    AnimatedVisibility(showContent) {
+                        val greeting = remember { Greeting().greet() }
+                        Column(
+                            Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(painterResource(Res.drawable.compose_multiplatform), null)
+                            Text("Compose: $greeting")
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
