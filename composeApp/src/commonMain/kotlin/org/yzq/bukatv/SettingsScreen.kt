c2b878fb0fcf1c0428c1b4eebfb85645048d5636
yzq <zhenqiang.yu@foxmail.com>
2025-07-06 13:07:06 +0800

    add 增加菜单项

--- composeApp/src/commonMain/kotlin/org/yzq/bukatv/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/yzq/bukatv/SettingsScreen.kt
@@ -1,65 +1,90 @@
 package org.yzq.bukatv
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowForward
+import androidx.compose.material.icons.automirrored.rounded.ArrowForwardIos
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.PlayLesson
+import androidx.compose.material.icons.filled.SettingsApplications
+import androidx.compose.material.icons.filled.SettingsCell
+import androidx.compose.material.icons.filled.Source
+import androidx.compose.material.icons.filled.Tune
+import androidx.compose.material.icons.filled.VideoSettings
+import androidx.compose.material.icons.rounded.ArrowCircleRight
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import bukatv.composeapp.generated.resources.Res
-import bukatv.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+data class SettingItem(val name: String, val icon: ImageVector, val onClick: () -> Unit);
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun SettingsScreen() {
+
+    val settings = listOf<SettingItem>(
+        SettingItem(name = "通用", icon = Icons.Default.Tune, onClick = {}),
+        SettingItem(name = "文件来源", icon = Icons.Default.Source, onClick = {}),
+        SettingItem(name = "播放设置", icon = Icons.Default.VideoSettings, onClick = {}),
+        SettingItem(name = "关于", icon = Icons.Rounded.Info, onClick = {}),
+    );
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        // 定义可变状态
-        var showContent by remember { mutableStateOf(false) }
 
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text("设置") }
-                )
-            },
-            content = { innerPadding ->
-                Column(
-                    modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Button(onClick = { showContent = !showContent }) {
-                        Text("Click me!")
-                    }
-                    AnimatedVisibility(showContent) {
-                        val greeting = remember { Greeting().greet() }
-                        Column(
-                            Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
+        Scaffold(topBar = {
+            TopAppBar(
+                title = { Text("设置") })
+        }, content = { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                settings.forEachIndexed { index, item ->
+                    Box(modifier = Modifier.fillMaxWidth().clickable { item.onClick() }) {
+                        Row(
+                            modifier = Modifier.padding(15.dp, 10.dp),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Image(painterResource(Res.drawable.compose_multiplatform), null)
-                            Text("Compose: $greeting")
+                            Icon(
+                                item.icon,
+                                contentDescription = item.name,
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(item.name, modifier = Modifier.weight(1f))
+                            Icon(
+                                Icons.AutoMirrored.Rounded.ArrowForwardIos,
+                                contentDescription = item.name,
+                            )
                         }
                     }
+                    Box(modifier = Modifier.fillMaxWidth().height(1.dp).background(Color.Gray))
                 }
             }
-        )
+        })
     }
 }
