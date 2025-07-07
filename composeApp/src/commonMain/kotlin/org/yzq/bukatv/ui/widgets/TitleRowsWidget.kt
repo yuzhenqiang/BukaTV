39a1e2f32985882d6277f2f11c7d892b6aa2a55c
yzq <zhenqiang.yu@foxmail.com>
2025-07-07 17:29:52 +0800

    update

--- composeApp/src/commonMain/kotlin/org/yzq/bukatv/ui/widgets/TitleRowsWidget.kt
+++ composeApp/src/commonMain/kotlin/org/yzq/bukatv/ui/widgets/TitleRowsWidget.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
